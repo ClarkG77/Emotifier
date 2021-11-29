e6f942b92f3877a920825e9a02ca10c0fecd74f8
ClarkG77 <clarkgodwin7@gmail.com>
2021-11-28 20:53:39 -0500

    Working on self blur

--- cartooninator.py
+++ cartooninator.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-from skimage.segmentation import slic
+import scipy
 from matplotlib import pyplot as pp
 
 # Environment Variables
@@ -18,5 +18,31 @@ def cartoonify(img):
 
 # guassian blur
 def blur(img):
+    sigma = 10
+    G = fgaussian(2 * np.ceil(3 * sigma) + 1, sigma)
+    gIm = np.zeros([len(img),len(img[0],3)])
+    gIm[0] = scipy.ndimage.convolve(img, G, mode="nearest")
+    gIm[1] = scipy.ndimage.convolve(img, G, mode="nearest")
+    gIm[2] = scipy.ndimage.convolve(img, G, mode="nearest")
+    return gIm
+def fgaussian(size, sig):
+    
+  #Prep for two dims
+  shape = (size,size)
 
-    return img
+  m,n = [(ss-1.)/2. for ss in shape]
+  y,x = np.ogrid[-m:m + 1, -n:n + 1]
+
+
+  h = np.exp( -(x * x + y * y) / (2. * sig * sig) )
+  h[ h < np.finfo(h.dtype).eps * h.max() ] = 0
+
+  sumh = h.sum()
+  if sumh != 0:
+    h /= sumh
+  return h
+
+im = pp.imread('images\\cat.jpg')
+imgC = cartoonify(im)
+pp.imshow(imgC)
+pp.show()
